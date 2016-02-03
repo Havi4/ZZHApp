fa289dbfaf50f0349e2f53e792646ebaf04149c8
Havi_li <1058695900@qq.com>
2016-02-03 12:11:30 +0800

    Initial Commit

---
+++ HaviProjectBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HaviProjectBase
+//
+//  Created by Havi on 16/2/3.
+//  Copyright © 2016年 Havi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
