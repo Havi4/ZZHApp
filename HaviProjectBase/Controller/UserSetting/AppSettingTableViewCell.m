ab92970d91cc6e420fe03776f66a7032f3c20505
Havi_li <1058695900@qq.com>
2016-09-28 14:52:12 +0800

    build master 设定修改

--- HaviProjectBase/Controller/UserSetting/AppSettingTableViewCell.m
+++ HaviProjectBase/Controller/UserSetting/AppSettingTableViewCell.m
@@ -39,6 +39,35 @@
         cell.textLabel.text = obj;
         cell.backgroundColor = [UIColor whiteColor];
     }
+    cell.imageView.contentMode = UIViewContentModeScaleAspectFill;
+    if (indexPath.section == 0 && indexPath.row == 0) {
+        cell.imageView.image = [UIImage imageNamed:@"toux1"];
+        UIView *line = [[UIView alloc] init];
+        line.backgroundColor = [UIColor lightGrayColor];
+        [self addSubview:line];
+        [line makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(self.mas_left);
+            make.right.equalTo(self.mas_right);
+            make.bottom.equalTo(self.mas_bottom).offset(0);
+            make.height.equalTo(@1);
+        }];
+    }else if (indexPath.section == 0 && indexPath.row == 1){
+        cell.imageView.image = [UIImage imageNamed:@"mim"];
+    }else if (indexPath.section == 1 && indexPath.row == 0){
+        cell.imageView.image = [UIImage imageNamed:@"xieyi"];
+        UIView *line = [[UIView alloc] init];
+        line.backgroundColor = [UIColor lightGrayColor];
+        [self addSubview:line];
+        [line makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(self.mas_left);
+            make.right.equalTo(self.mas_right);
+            make.bottom.equalTo(self.mas_bottom).offset(0);
+            make.height.equalTo(@1);
+        }];
+
+    }else if (indexPath.section == 1 && indexPath.row == 1){
+        cell.imageView.image = [UIImage imageNamed:@"guanyu"];
+    }
     cell.selectionStyle = UITableViewCellSelectionStyleGray;
 }
 @end
