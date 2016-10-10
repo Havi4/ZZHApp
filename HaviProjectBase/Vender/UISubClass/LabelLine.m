260d874ee87761c773ada0c5b070936592cf8593
Havi_Lee <1058695900@qq.com>
2016-10-10 15:26:32 +0800

    build master 首页下划线

--- HaviProjectBase/Vender/UISubClass/LabelLine.m
+++ HaviProjectBase/Vender/UISubClass/LabelLine.m
@@ -25,27 +25,27 @@
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    CGSize fontSize =[self.text sizeWithFont:self.font
-                                    forWidth:self.frame.size.width
-                               lineBreakMode:NSLineBreakByTruncatingTail];
-    
-    CGContextSetStrokeColorWithColor(ctx, self.textColor.CGColor);  // set as the text's color
-    CGContextSetLineWidth(ctx, 1.0f);
-    
-    CGPoint l,r;
-        l = CGPointMake(0, self.frame.size.height/2.0 +fontSize.height/2.0);
-        r = CGPointMake(fontSize.width, self.frame.size.height/2.0 + fontSize.height/2.0);
-        
-    // Add Move Command to point the draw cursor to the starting point
-    CGContextMoveToPoint(ctx, l.x, l.y);
-    
-    // Add Command to draw a Line
-    CGContextAddLineToPoint(ctx, r.x, r.y);
-    
-    
-    // Actually draw the line.
-    CGContextStrokePath(ctx);
+//    CGContextRef ctx = UIGraphicsGetCurrentContext();
+//    CGSize fontSize =[self.text sizeWithFont:self.font
+//                                    forWidth:self.frame.size.width
+//                               lineBreakMode:NSLineBreakByTruncatingTail];
+//    
+//    CGContextSetStrokeColorWithColor(ctx, self.textColor.CGColor);  // set as the text's color
+//    CGContextSetLineWidth(ctx, 1.0f);
+//    
+//    CGPoint l,r;
+//        l = CGPointMake(0, self.frame.size.height/2.0 +fontSize.height/2.0);
+//        r = CGPointMake(fontSize.width, self.frame.size.height/2.0 + fontSize.height/2.0);
+//        
+//    // Add Move Command to point the draw cursor to the starting point
+//    CGContextMoveToPoint(ctx, l.x, l.y);
+//    
+//    // Add Command to draw a Line
+//    CGContextAddLineToPoint(ctx, r.x, r.y);
+//    
+//    
+//    // Actually draw the line.
+//    CGContextStrokePath(ctx);
     
     // should be nothing, but who knows...
     [super drawRect:rect];
