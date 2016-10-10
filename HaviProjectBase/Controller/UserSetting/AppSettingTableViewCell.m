43b58dd2070bce981c956e1f752f9938995b6af1
Havi_Lee <1058695900@qq.com>
2016-10-10 15:22:23 +0800

    build master 设定界面

--- HaviProjectBase/Controller/UserSetting/AppSettingTableViewCell.m
+++ HaviProjectBase/Controller/UserSetting/AppSettingTableViewCell.m
@@ -8,7 +8,27 @@
 
 #import "AppSettingTableViewCell.h"
 
+@interface AppSettingTableViewCell ()
+@property (nonatomic, strong) UILabel *title;
+@end
+
 @implementation AppSettingTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        _title = [[UILabel alloc]init];
+        _title.textColor = [UIColor colorWithRed:0.016 green:0.020 blue:0.024 alpha:1.00];
+        _title.font = [UIFont systemFontOfSize:16];
+        [self addSubview:_title];
+        [_title makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(self.mas_left).offset(46);
+            make.centerY.equalTo(self.mas_centerY);
+        }];
+    }
+    return self;
+}
 
 - (void)configure:(UITableViewCell *)cell
         customObj:(id)obj
@@ -18,12 +38,15 @@
     if (indexPath.section == 2) {
         cell.accessoryType = UITableViewCellAccessoryNone;
         cell.backgroundColor = [UIColor whiteColor];
+        self.title.hidden = YES;
         cell.textLabel.text = obj;
         cell.textLabel.textAlignment = NSTextAlignmentCenter;
         cell.textLabel.textColor = [UIColor colorWithRed:0.502 green:0.827 blue:1.000 alpha:1.00];
     }else if (indexPath.section == 1 && indexPath.row == 1){
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        cell.textLabel.text = obj;
+//        cell.textLabel.text = obj;
+        self.title.text = obj;
+        self.title.hidden = NO;
         cell.backgroundColor = [UIColor whiteColor];
         UILabel *versionLabel = [[UILabel alloc]init];
         versionLabel.text = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
@@ -36,20 +59,21 @@
         }];
     }else{
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        cell.textLabel.text = obj;
+        self.title.text = obj;
         cell.backgroundColor = [UIColor whiteColor];
     }
-    cell.imageView.contentMode = UIViewContentModeScaleAspectFill;
+    cell.imageView.contentMode = UIViewContentModeScaleToFill;
     if (indexPath.section == 0 && indexPath.row == 0) {
         cell.imageView.image = [UIImage imageNamed:@"toux1"];
         UIView *line = [[UIView alloc] init];
         line.backgroundColor = [UIColor lightGrayColor];
+        line.alpha = 0.3;
         [self addSubview:line];
         [line makeConstraints:^(MASConstraintMaker *make) {
             make.left.equalTo(self.mas_left);
             make.right.equalTo(self.mas_right);
             make.bottom.equalTo(self.mas_bottom).offset(0);
-            make.height.equalTo(@1);
+            make.height.equalTo(@0.5);
         }];
     }else if (indexPath.section == 0 && indexPath.row == 1){
         cell.imageView.image = [UIImage imageNamed:@"mim"];
@@ -57,12 +81,13 @@
         cell.imageView.image = [UIImage imageNamed:@"xieyi"];
         UIView *line = [[UIView alloc] init];
         line.backgroundColor = [UIColor lightGrayColor];
+        line.alpha = 0.3;
         [self addSubview:line];
         [line makeConstraints:^(MASConstraintMaker *make) {
             make.left.equalTo(self.mas_left);
             make.right.equalTo(self.mas_right);
             make.bottom.equalTo(self.mas_bottom).offset(0);
-            make.height.equalTo(@1);
+            make.height.equalTo(@0.5);
         }];
 
     }else if (indexPath.section == 1 && indexPath.row == 1){
